557bfa696943e68be64e9d5dc0c2a7caa31e0733
Luis Fernando Robles Ibarra <lfrincy@hotmail.com>
2026-01-20 23:26:46 -0600

    instalación de laravel

---
+++ mensaje-cifrado/resources/js/bootstrap.js
@@ -0,0 +1,23 @@
+import _ from 'lodash'
+window._ = _
+
+import axios from 'axios'
+window.axios = axios
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+/**
+ * Echo (opcional)
+ * Si NO usas realtime, puedes dejar esto comentado.
+ */
+
+// import Echo from 'laravel-echo'
+// import Pusher from 'pusher-js'
+// window.Pusher = Pusher
+
+// window.Echo = new Echo({
+//   broadcaster: 'pusher',
+//   key: import.meta.env.VITE_PUSHER_APP_KEY,
+//   cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+//   forceTLS: true,
+// })
