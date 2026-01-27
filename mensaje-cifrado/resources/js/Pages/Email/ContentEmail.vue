ad2faf5278597a85f104608b0e538e60bd3c48f1
Luis Fernando Robles Ibarra <lfrincy@hotmail.com>
2026-01-26 22:03:25 -0600

    Avance UI vista home

---
+++ mensaje-cifrado/resources/js/Pages/Email/ContentEmail.vue
@@ -0,0 +1,55 @@
+<template>
+    <section v-if="modelValue" @click.self="closeModal" class="absolute inset-0 flex items-center justify-center bg-gray-400/50">
+        <div class="w-96 h-auto bg-white shadow-xl rounded-xl p-5">
+            <header class="flex items-center justify-end mb-6">
+                <button class="w-8 h-8 bg-black/5 flex items-center justify-center rounded-full backdrop-blur-md" @click="closeModal">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+                </button>
+            </header>
+
+            <div class="h-auto max-h-52 overflow-y-auto">
+                <p class=" sticky top-0 font-semibold bg-white mb-6">
+                    Asunto: 
+                    <span class="font-normal">
+                        {{  email?.subject || 'No se encontró el asunto del correo'  }}
+                    </span>
+                </p>
+                <p class="font-semibold">
+                    Contenido del correo
+                    <span class="font-normal">
+                        {{  email?.content || 'No se encontró el contenido del correo'  }}
+                    </span>
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue: {
+            type: Boolean,
+            required: true
+        },
+        email: {
+            type: Object,
+            required: true
+        },
+    },
+    data() {
+        return {
+            dumpEmail: {
+                subject: 'Apple support',
+                content: 'Esto es el contenido dump del correo electrónico cifrado.'
+            }
+        }
+    },
+    emits: ["update:modelValue"],
+    methods: {
+        closeModal() {
+            this.$emit('update:modelValue', false);
+        }
+    }
+}
+</script>
---
+++ mensaje-cifrado/resources/js/Pages/Email/Home.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="h-screen bg-gray-100">
+        <header class="h-16 w-full flex items-center justify-between p-5">
+
+            <div class="flex items-center gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 10a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+                <el-input v-model="search" placeholder="Buscar correo"></el-input>
+            </div>
+
+            <el-dropdown>
+                <div class="flex flex-col gap-2">
+                    <img class="w-8 h-8 rounded-full" :src="'img/user-default.png'" alt="Usuario por defecto">
+                </div>
+                
+                <template #dropdown>
+                <el-dropdown-menu>
+                    <el-dropdown-item>Action 1</el-dropdown-item>
+                </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </header>
+        <main class="h-full w-full bg-white rounded-xl">
+            <div class="p-4">
+                <button 
+                :disabled="isLoading"
+                :class="isLoading ? 'text-gray-400 spinner' : 'text-gray-900'"
+                @click="fetchEmails"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 icon icon-tabler icons-tabler-outline icon-tabler-reload"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747" /><path d="M20 4v5h-5" /></svg>
+                </button>
+            </div>
+            <template v-for="(value, index) in dumpEmails" :key="index">
+                <article class="grid grid-cols-[150px_1fr_100px] p-4 text-gray-900 text-sm border-b border-gray-300 hover:bg-gray-200 hover:cursor-pointer" @click="openEmail(value)">
+                    <h6 class="font-semibold">{{ value.subject }}</h6>
+                    <p class="text-gray-500">Haz clic para ver el contenido del mensaje...</p>
+                    <span class="font-semibold">{{value.timestamp}}</span>
+                </article>
+            </template>
+        </main>
+    </div>
+
+    <Transition>
+        <ContentEmail v-model="openModalEmail" :email="currentEmail" />
+    </Transition>
+</template>
+
+<script>
+import ContentEmail from './ContentEmail.vue';
+    export default {
+        components: {
+            ContentEmail,
+        },
+        props: {
+
+        },
+
+        data() {
+            return {
+                search: '',
+                isLoading: false,
+
+                openModalEmail: false,
+                currentEmail: {},
+                dumpEmails: [
+                    {
+                        subject: 'Apple support',
+                        content: 'Esto es el contenido dump del correo electrónico cifrado.',
+                        timestamp: '10:30 AM'
+                    },
+                    {
+                        subject: 'Google support',
+                        content: 'Esto es el contenido dump del correo electrónico cifrado.',
+                        timestamp: '10:30 AM'
+                    },
+                    {
+                        subject: 'Microsoft support',
+                        content: 'Esto es el contenido dump del correo electrónico cifrado.',
+                        timestamp: '10:30 AM'
+                    },
+                ]
+            }
+        },
+        methods: {
+            async fetchEmails() {
+                this.isLoading = true;
+
+            },
+            openEmail(email) {
+                this.openModalEmail = true;
+                this.currentEmail = email;
+            }      
+        }
+    }
+</script>
+
+<style>
+    .spinner {
+        animation: spin 1.5s linear infinite;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+
+    .v-enter-active,
+    .v-leave-active {
+    transition: opacity 0.5s ease;
+    }
+
+    .v-enter-from,
+    .v-leave-to {
+    opacity: 0;
+    }
+</style>
