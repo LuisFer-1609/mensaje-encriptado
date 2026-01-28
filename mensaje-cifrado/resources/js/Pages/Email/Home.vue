73a4301c4f6a2a5e1f4a3feed39dbb3ecf453005
Luis Fernando Robles Ibarra <lfrincy@hotmail.com>
2026-01-27 19:52:56 -0600

    Update Home.vue

--- mensaje-cifrado/resources/js/Pages/Email/Home.vue
+++ mensaje-cifrado/resources/js/Pages/Email/Home.vue
@@ -19,24 +19,31 @@
                 </template>
             </el-dropdown>
         </header>
-        <main class="h-full w-full bg-white rounded-xl">
-            <div class="p-4">
-                <button 
-                :disabled="isLoading"
-                :class="isLoading ? 'text-gray-400 spinner' : 'text-gray-900'"
-                @click="fetchEmails"
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 icon icon-tabler icons-tabler-outline icon-tabler-reload"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747" /><path d="M20 4v5h-5" /></svg>
-                </button>
-            </div>
-            <template v-for="(value, index) in dumpEmails" :key="index">
-                <article class="grid grid-cols-[150px_1fr_100px] p-4 text-gray-900 text-sm border-b border-gray-300 hover:bg-gray-200 hover:cursor-pointer" @click="openEmail(value)">
-                    <h6 class="font-semibold">{{ value.subject }}</h6>
-                    <p class="text-gray-500">Haz clic para ver el contenido del mensaje...</p>
-                    <span class="font-semibold">{{value.timestamp}}</span>
-                </article>
-            </template>
-        </main>
+        <section class="flex flex-row">
+            <aside class="p-5">
+                <ul>
+                    <li>Bandeja de entrada</li>
+                </ul>
+            </aside>
+            <main class="h-full w-full bg-white rounded-xl">
+                <div class="p-4">
+                    <button 
+                    :disabled="isLoading"
+                    :class="isLoading ? 'text-gray-400 spinner' : 'text-gray-900'"
+                    @click="fetchEmails"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 icon icon-tabler icons-tabler-outline icon-tabler-reload"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747" /><path d="M20 4v5h-5" /></svg>
+                    </button>
+                </div>
+                <template v-for="(value, index) in dumpEmails" :key="index">
+                    <article class="grid grid-cols-[150px_1fr_100px] p-4 text-gray-900 text-sm border-b border-gray-300 hover:bg-gray-200 hover:cursor-pointer" @click="openEmail(value)">
+                        <h6 class="font-semibold">{{ value.subject }}</h6>
+                        <p class="text-gray-500">Haz clic para ver el contenido del mensaje...</p>
+                        <span class="font-semibold">{{value.timestamp}}</span>
+                    </article>
+                </template>
+            </main>
+        </section>
     </div>
 
     <Transition>
