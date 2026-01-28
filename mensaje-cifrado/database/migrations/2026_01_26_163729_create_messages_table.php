9b4e64ecea4af868b7518e2456399779c33eb268
Anthony Fuentes Carrera <134445754+anthonyfs77@users.noreply.github.com>
2026-01-28 00:32:16 -0600

    Update 2026_01_26_163729_create_messages_table.php
    Controler register actualizado

--- mensaje-cifrado/database/migrations/2026_01_26_163729_create_messages_table.php
+++ mensaje-cifrado/database/migrations/2026_01_26_163729_create_messages_table.php
@@ -11,10 +11,20 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
+        
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('recipient_id')->constrained('users')->onDelete('cascade');
+            // Mensaje
+            $table->string('subject');
+            // Mensaje encriptado
+            $table->longText('body'); 
+            // Estatus ('sent', 'read')
+            $table->boolean('status')->default(0);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
